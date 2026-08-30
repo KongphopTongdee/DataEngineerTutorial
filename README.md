a5c688c29542a32ede2041bfac3920bae4e7d765
Kongphop Tongdee <kongphop666@Kongphops-MacBook.local>
2026-08-30 22:34:19 +0700

    Create extracted_video_data which was be populating in our data warehouse( prepare data for other people )

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 
 1. Open your terminalNavigate to the directory where your virtual environment folder (e.g., .venv or my_env) is located.
 
-2.Activate the environmentRun the appropriate activation command for your operating system and shell: For macOS / Linux ( Bash/Zsh ) : source .venv/bin/activate
+2. Activate the environmentRun the appropriate activation command for your operating system and shell: For macOS / Linux ( Bash/Zsh ) : source .venv/bin/activate
--- video_stats.py
+++ video_stats.py
@@ -97,6 +97,49 @@ def get_video_ids( playlistID ):
     except requests.exceptions.RequestException as e:
         raise e
 
+def extract_video_data( video_ids ):
+    extracted_data = []
+
+    def batch_list( video_id_lst, batch_size ):
+        for video_id in range( 0, len( video_id_lst ), batch_size ):
+            # Explain yield function in the goodnote
+            yield video_id_lst[ video_id: video_id + batch_size ]
+
+    try:
+        for batch in batch_list( video_ids, maxResult ):
+            video_ids_str = ",".join(batch)
+
+            url = f"https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails&part=snippet&part=statistics&id={video_ids_str}&key={API_KEY}"
+
+            responseData = requests.get( url )
+            responseData.raise_for_status()
+    
+            dataJson = responseData.json()
+
+            # Get the output in the json format
+            for item in dataJson.get("items", []):
+                video_id = item["id"]
+                snippet = item[ "snippet" ]
+                contentDetails = item["contentDetails"] 
+                statistics = item["statistics"]
+
+                # Create new data to easy understand on my own
+                video_data = {
+                    "video_id": video_id, 
+                    "title": snippet["title"],
+                    "publishedAt": snippet["publishedAt"],
+                    "duration": contentDetails["duration"],
+                    "viewCount": statistics.get("viewCount", None),
+                    "likeCount": statistics.get("likeCount", None),
+                    "commentCount": statistics.get("commentCount", None)
+                }
+
+                extracted_data.append( video_data )
+
+        return extracted_data
+        
+    except requests.exceptions.RequestException as e:
+        raise e
 
 ########################################################
 #
@@ -113,7 +156,7 @@ def get_video_ids( playlistID ):
 def main():
     playlistID = get_playlist_ID()
     videoIDs = get_video_ids( playlistID )
-    print( videoIDs )
+    print( extract_video_data( videoIDs )  )
 
 if __name__ == '__main__':
     main()

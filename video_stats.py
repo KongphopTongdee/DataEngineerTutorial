9529e9eb555e0745c7d517077b9df80f1a5ad9ed
Kongphop Tongdee <kongphop666@Kongphops-MacBook.local>
2026-08-30 17:56:39 +0700

    Create the get video ids function in the vido_stats file

--- video_stats.py
+++ video_stats.py
@@ -31,7 +31,7 @@ API_KEY = os.getenv("API_KEY")
 
 CHANNEL_HANDLE = "MrBeast"
 
-url = f"https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDLE}&key={API_KEY}"
+maxResult = 50
 
 ########################################################
 #
@@ -44,6 +44,8 @@ def convertPythonToJson( data ):
     return answer
 
 def get_playlist_ID():
+    url = f"https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDLE}&key={API_KEY}"
+
     try:
         responseData = requests.get( url )
         responseData.raise_for_status()
@@ -56,8 +58,42 @@ def get_playlist_ID():
         # Get the output from the json
         channel_items = dataJson["items"][0]
         channel_playlistID = channel_items["contentDetails"]["relatedPlaylists"]["uploads"]
-        print( channel_playlistID )
+        # print( channel_playlistID )
         return channel_playlistID
+    except requests.exceptions.RequestException as e:
+        raise e
+ 
+def get_video_ids( playlistID ):
+    base_url = f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={maxResult}&playlistId={ playlistID }&key={API_KEY}"
+
+    pageToken = None
+
+    video_ids =[]
+
+    try:
+
+        while True :
+            url = base_url
+
+            if pageToken:
+                url += f"&pageToken={pageToken}"
+
+            responseData = requests.get( url )
+            responseData.raise_for_status()
+    
+            dataJson = responseData.json()
+
+            for item in dataJson.get( "items", [] ):
+                video_id = item["contentDetails"]["videoId"]
+                video_ids.append( video_id )
+
+            pageToken = dataJson.get("nextPageToken")
+
+            if not pageToken:
+                break
+
+        return video_ids  
+
     except requests.exceptions.RequestException as e:
         raise e
 
@@ -75,7 +111,9 @@ def get_playlist_ID():
 #
 
 def main():
-    get_playlist_ID()
+    playlistID = get_playlist_ID()
+    videoIDs = get_video_ids( playlistID )
+    print( videoIDs )
 
 if __name__ == '__main__':
     main()

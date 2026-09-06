3230689229d41c0d8c552abbe76fa96cf9f0ba11
Kongphop Tongdee <kongphop666@Kongphops-MacBook.local>
2026-09-06 21:51:16 +0700

    Create def function including create_schema, create_table, get_video_ids in data_utils.py file

--- dags/datawarehouse/data_utils.py
+++ dags/datawarehouse/data_utils.py
@@ -24,7 +24,7 @@ from pyscopg2.extras import RealDictCursor
 #	GLOBALS
 #
 
-
+table = "yt_api"
 
 ########################################################
 #
@@ -40,6 +40,50 @@ def get_conn_cursor():
 def close_conn_cursor( conn, cur ):
     cur.close()
     conn.close()
+
+def create_schema( schema ):
+    conn, cur = get_conn_cursor()
+    schema_sql = f"CREATE SCHEMA IF NOT EXISTS {schema};"
+    cur.execute( schema_sql )
+    conn.commit()
+    close_conn_cursor()
+
+def create_table( schema ):
+    conn, cur = get_conn_cursor()
+    if schema == "staging":
+        table_sql = f"""
+                CREATE TABLE IF NOT EXISTS {schema}.{table} (
+                    "Video_ID" VARCHAR(11) PRIMARY KEY NOT NULL,
+                    "Video_Title" TEXT NOT NULL,
+                    "Upload_Date" TIMESTAMP NOT NULL,
+                    "Duration" VARCHAR(20) NOT NULL,
+                    "Video_Views" INT,
+                    "Likes_Count" INT,
+                    "Comments_Count" INT
+                );
+            """
+    else:
+        table_sql = f"""
+                CREATE TABLE IF NOT EXISTS {schema}.{table} (
+                    "Video_ID" VARCHAR(11) PRIMARY KEY NOT NULL,
+                    "Video_Title" TEXT NOT NULL,
+                    "Upload_Date" TIMESTAMP NOT NULL,
+                    "Duration" TIME NOT NULL,
+                    "Video_Type" VARCHAR(10) NOT NULL,
+                    "Video_Views" INT,
+                    "Likes_Count" INT,
+                    "Comments_Count" INT
+                );
+            """
+    cur.excute( table_sql )
+    conn.commit()
+    close_conn_cursor()
+
+def get_video_ids( cur, schema ):
+    cur.execute( f"""SELECT "Video_ID" FROM {schema}.{table};""" )
+    ids = cur.fetchall()
+    video_ids = [row["Video_ID"] for row in ids]
+    return video_ids
 
 ########################################################
 #

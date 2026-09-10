53fa4c9d8320ab2203379036b84d17fa3cf02fec
Kongphop Tongdee <kongphop666@Kongphops-MacBook.local>
2026-09-10 17:22:47 +0700

    Update the DAGs main.py to press only 1 trigger for run continue DAGS, Update the unit_test.py expected_task_counts +1 on task produce_json and update_db

--- dags/main.py
+++ dags/main.py
@@ -14,6 +14,8 @@ import pendulum
 
 from datetime import datetime, timedelta
 
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+
 ########################################################
 #
 #	LOCAL IMPORTS
@@ -77,7 +79,7 @@ with DAG(
     description = "DAG to produce JSON file with raw data",
     schedule="0 14 * * *",
     catchup=False
-) as dag:
+) as dag_produce:
 
     # Define tasks
     playlist_id = get_playlist_ID()
@@ -85,31 +87,41 @@ with DAG(
     extract_data = extract_video_data( video_ids )
     save_to_json_task = save_to_json( extract_data )
 
+    trigger_update_db = TriggerDagRunOperator(
+        task_id="trigger_update_db",
+        trigger_dag_id="update_db",
+    )
+
     # Define dependencies
-    playlist_id >> video_ids >> extract_data >> save_to_json_task
+    playlist_id >> video_ids >> extract_data >> save_to_json_task >> trigger_update_db
 
 with DAG(
     dag_id="update_db",
     default_args = default_args,
     description = "DAG to produce JSON file and insert data into both staging and core schemas",
-    schedule="0 15 * * *",
+    schedule=None,
     catchup=False
-) as dag:
+) as dag_update:
 
     # Define tasks
     update_staging = staging_table()
     update_core = core_table()
 
+    trigger_data_quality = TriggerDagRunOperator(
+        task_id="trigger_data_quality",
+        trigger_dag_id="data_quality",
+    )
+
     # Define dependencies
-    update_staging >> update_core
+    update_staging >> update_core >> trigger_data_quality
 
 with DAG(
     dag_id="data_quality",
     default_args = default_args,
     description = "DAG to check the data quality on both layers in the db",
-    schedule="0 16 * * *",
+    schedule=None,
     catchup=False
-) as dag:
+) as dag_quality:
 
     # Define tasks
     soda_validate_staging = yt_elt_data_quality( staging_schema )
